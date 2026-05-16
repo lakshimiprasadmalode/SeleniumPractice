8ef48c49551258145fcc124f8759567192ad484b
lakshimiprasad <114915459+lakshimiprasad@users.noreply.github.com>
2026-05-16 08:51:40 +0530

    Adding for scm polling

--- src/main/java/com/qa/pom/AmazonHomePage.java
+++ src/main/java/com/qa/pom/AmazonHomePage.java
@@ -7,18 +7,22 @@ import org.openqa.selenium.support.PageFactory;
 
 public class AmazonHomePage {
 	
-
+  //Locators
 	@FindBy(css="#twotabsearchtextbox:first-of-type")
 	private WebElement searchbox;
 	
 	@FindBy(css="#nav-search-submit-button")
 	private WebElement searchbutton;
 	
+	
+  //constructor 	
 	public AmazonHomePage(WebDriver driver) {
 	
 		PageFactory.initElements(driver, this);
 	}
 	
+	
+	//Implementation Methods
 	public void searchitem(String item) {
 		searchbox.sendKeys(item);
 		searchbutton.click();
